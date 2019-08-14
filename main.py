bcfb8b5f9c2bbe349032a5cf8d80d18bbfcea819
hammy3502 <hammy275@gmail.com>
2019-08-14 13:56:07 -0400

    Progress bars and auto-setup for ubuntu distros

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from shutil import which,rmtree
 import sys
 import os
 from platform import system
+from time import sleep
 
 try:
     import PySimpleGUI as sg
@@ -26,14 +27,61 @@ except ModuleNotFoundError:
     print("PySimpleGUI not installed! Please run python3 -m pip install PySimpleGUI before starting this program!")
     sys.exit(1)
 
+def scrcpy_install_linux():
+    print("Checking for root...")
+    if os.getuid() != 0:
+        sg.Popup("Root is required to complete this first time setup! Please run this script as root!")
+        sys.exit(1)
+    else:
+        print("Creating progress window")
+        bar_layout = [
+            [sg.Text("Preparing scrcpy and adb...")],
+            [sg.ProgressBar(100, orientation='h', size=(20, 20), key='installbar')]
+        ]
+        try:
+            import distro
+        except ModuleNotFoundError:
+            sg.Popup("Please install distro with the command python3 -m pip install distro!")
+            sys.exit(1)
+        install_window = sg.Window("Installing...", bar_layout)
+        bar = install_window.FindElement("installbar")
+        install_window.Read(timeout=0)
+        print("Installing scrcpy and ADB...")
+        bar.UpdateBar(1)
+        dist = distro.linux_distribution(full_distribution_name=False)
+        bar.UpdateBar(3)
+        dist = ["xubuntu", 1, 2]
+        if dist[0] in ["linuxmint"] or "ubuntu" in dist[0]:
+            print("Detected Ubuntu-based system!")
+            print("Installing scrcpy through snap")
+            bar.UpdateBar(50)
+            os.system("snap install scrcpy")
+            bar.UpdateBar(100)
+            sg.Popup("Install complete! Please run scrcpy-gui as your intended user (usually not root).")
+            sys.exit(0)
+        else:
+            sg.Popup("Distro does not support automatic install! Please manually install adb and scrcpy!")
+            sys.exit(1)
+
+
 def scrcpy_install_win():
+    print("Creating progress window")
+    bar_layout = [
+        [sg.Text("Preparing scrcpy and adb...")],
+        [sg.ProgressBar(100, orientation='h', size=(20, 20), key='installbar')]
+    ]
+    install_window = sg.Window("Installing...", bar_layout)
+    bar = install_window.FindElement("installbar")
+    install_window.Read(timeout=0)
     print("Installing scrcpy and ADB...")
     os.chdir(os.path.expandvars("%temp%"))
+    bar.UpdateBar(1)
     print("Removing old scrcpy-gui-setup folder (if it exists!).")
     try:
         rmtree("scrcpy-gui-setup")
     except FileNotFoundError:
         pass
+    bar.UpdateBar(2)
     os.mkdir("scrcpy-gui-setup")
     is_64bits = sys.maxsize > 2**32
     os.chdir(os.path.expandvars("%temp%/scrcpy-gui-setup"))
@@ -43,6 +91,7 @@ def scrcpy_install_win():
         print("Requests module isn't installed!")
         sg.Popup("requests isn't installed! Please run python -m pip install PySimpleGUI before starting this program!")
         sys.exit(1)
+    bar.UpdateBar(3)
     print("Downloading scrcpy zip...")
     if is_64bits:
         print("Choosing 64-bit version")
@@ -50,7 +99,9 @@ def scrcpy_install_win():
     else:
         print("Choosing 32-bit version.")
         f = requests.get("https://github.com/Genymobile/scrcpy/releases/download/v1.10/scrcpy-win32-v1.10.zip")
+    bar.UpdateBar(4)
     open("scrcpy.zip", "wb").write(f.content)
+    bar.UpdateBar(60)
     scrcpy_install = os.path.expandvars("%userprofile%/scrcpy")
     print("Extracting ZIP to {}".format(scrcpy_install))
     try:
@@ -65,20 +116,27 @@ def scrcpy_install_win():
             rmtree(os.path.expandvars(scrcpy_install))
         except FileNotFoundError:
             pass
+        bar.UpdateBar(65)
         print("Doing ZIP extract...")
         os.mkdir(scrcpy_install)
         os.chdir(scrcpy_install)
         z_file.extractall(os.path.expandvars(scrcpy_install))
-
+        bar.UpdateBar(100)
 
 if (which("adb") == None or which("scrcpy") == None) and not(os.path.isfile(os.path.expandvars("%userprofile%/scrcpy/scrcpy.exe"))):
     print("ADB/scrcpy not installed!")
     osys = system()
-    osys = "Windows"
     if osys == "Windows":
         response = sg.PopupYesNo("Would you like to automatically install ADB and scrcpy? By doing so, you agree to any and all license agreements that come with those pieces of software!")
         if response == "Yes":
             scrcpy_install_win()
+        else:
+            print("Not installing! Exiting...")
+            sys.exit(1)
+    elif osys == "Linux":
+        response = sg.PopupYesNo("Would you like to automatically install ADB and scrcpy? By doing so, you agree to any and all license agreements that come with those pieces of software!")
+        if response == "Yes":
+            scrcpy_install_linux()
         else:
             print("Not installing! Exiting...")
             sys.exit(1)
@@ -144,7 +202,13 @@ print("Killing ADB server...") #Kill any previous ADB servers, just in case.
 call(["adb", "kill-server"])
 
 sg.Popup("Plug in your phone, then click Ok!")
-
+adb_layout = [
+    [sg.Text("Connecting to phone and launching scrcpy...")],
+    [sg.ProgressBar(100, orientation='h', size=(20, 20), key='adbbar')]
+]
+adb_window = sg.Window("Launching...", adb_layout)
+bar = adb_window.FindElement("adbbar")
+adb_window.Read(timeout=0)
 if values["wifi_mode"]:
     print("Wi-Fi preparation.")
     if values['addr'] == '':
@@ -152,14 +216,21 @@ if values["wifi_mode"]:
         sys.exit(1)
     else:
         call(["adb", "shell", "exit"])
+        bar.UpdateBar(10)
         connect_to = str(values["addr"] + ":5555") #Custom port may go here at some point
+        bar.UpdateBar(20)
         call(["adb", "tcpip", "5555"]) #Custom port may go here at some point
+        bar.UpdateBar(30)
+        sleep(2) #Waiting here can help things a decent amount of times
+        bar.UpdateBar(35)
         call(["adb", "connect", connect_to])
+        bar.UpdateBar(45)
         sg.Popup("Unplug your phone, then click Ok!")
 
 
 command = ["scrcpy"]
 print("Running scrcpy command...")
+bar.UpdateBar(46)
 
 if values["use_resolution"] and values["resolution"] != "":
     command.append("-m " + values["resolution"])
@@ -177,5 +248,6 @@ if values["sleep_screen"]:
     command.append("-S")
 if values["on_top"]:
     command.append("-T") #Assemble command
+bar.UpdateBar(100)
 print("Running command: " + " ".join(command))
 sys.exit(call(command)) #Run scrcpy command and give the exit code scrcpy gives us
